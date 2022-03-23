e1c4aad7fc42708034c769168aa24f39e2f6f656
Jonas Lara <adrianlr2515@outlook.com>
2022-03-23 13:38:33 -0600

    opencv

--- OpenCV/opencvtest1/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/adria/Artificial-Intelligence-CPP/OpenCV/opencvtest1
-# Build directory: C:/Users/adria/Artificial-Intelligence-CPP/OpenCV/opencvtest1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
